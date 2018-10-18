d845d7db1ef77635ed54bac3dd497cdb5212d803
talha javed <talhajaved67@gmail.com>
2018-10-18 09:05:13 +0500

    user roles added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ActiveRecord::Base
 	validates :password, presence: true, numericality: { only_integer: true }
 	has_one :article, dependent: :destroy
 	has_many :comments, dependent: :destroy
+	has_and_belongs_to_many :roles
 	before_create do
     self.username = username.downcase
-  end
-  def to_param 
-    username
-  end
+  	end
+  	
+  	def to_param 
+    	username
+  	end
 end
---
+++ db/migrate/20181018030826_create_roles_users.rb
@@ -0,0 +1,9 @@
+class CreateRolesUsers < ActiveRecord::Migration
+  def change
+    create_table :roles_users, id: false do |t|
+    	t.belongs_to :user, index: true
+    	t.belongs_to :role, index: true
+      t.timestamps null: false
+    end
+  end
+end
